b7eb9d6e2acea83bd8606842a3fb56e47e71d06d
Shakya Perera <ShakyaPerera111@gmail.com>
2022-06-09 20:05:07 +0530

    add dto

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  pols:Array<number>=[ ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  createPol(){
+    this.pols.push(1);
   }
 
 }
---
+++ src/app/dto/poll.ts
@@ -0,0 +1,4 @@
+export class Poll {
+  constructor(id:number,title:string,createBy:string,upVote:number=0,downVote:number=0) {
+  }
+}
